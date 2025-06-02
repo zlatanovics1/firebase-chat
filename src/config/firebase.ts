31eef947fa456ca776b5c2843a3817f7a3a31d7d
Strahinja Zlatanovic <office@zlatanovics.com>
2025-06-02 17:21:31 +0200

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -11,15 +11,15 @@ import { getMessaging } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const vapidKey =
-  "BHGjYPWsn_n3fG26WxHq97kLp8FHmUPWMXTm97luO3GGfV4EqM78VO9sxEoSkTd6a-BY3VcBPcmxxMqV7Oe4vMo";
+  "yourvapidkey";
 const firebaseConfig = {
-  apiKey: "AIzaSyD_C2R5WqVpRT_siOQlUX9s7IYwvfn4GHM",
-  authDomain: "firebasics-b0106.firebaseapp.com",
-  projectId: "firebasics-b0106",
-  storageBucket: "firebasics-b0106.appspot.com",
-  messagingSenderId: "41466439381",
-  appId: "1:41466439381:web:711c21bb90309d1282e45c",
-  measurementId: "G-64H9GG2D5S",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ":",
+  measurementId: "",
 };
 
 // Initialize Firebase
